ac165fe076ca960c4f0e3307f53fb801789d3207
huisoido <584331125@qq.com>
2017-08-10 13:35:09 +0800

    robotSearchPath

--- arithmetic/robotSearchPath/Robot.class.php
+++ arithmetic/robotSearchPath/Robot.class.php
@@ -5,11 +5,8 @@
 //要求：显示机器人第十二次移动的路径(第几次同理)
 
 class Robot {
-
-    public $path; //移动路径描述
     public $x; //坐标x
     public $y; //坐标y
-    public $direction; //移动方向
 
     function __construct() {
         $this->init();
@@ -17,17 +14,7 @@ class Robot {
 
     //初始化数值
     function init() {
-        $this->path = [];
         $this->x = $this->y = 0;
-        array_push($this->path, [
-            "x" => $this->x, "y" => $this->y
-        ]);
-        $this->direction = [
-            ["x" => $this->x - 1, "y" => $this->y],
-            ["x" => $this->x + 1, "y" => $this->y],
-            ["x" => $this->x, "y" => $this->y - 1],
-            ["x" => $this->x, "y" => $this->y + 1],
-        ];
     }
 
     //机器人向左移动的能力
@@ -48,12 +35,5 @@ class Robot {
     //机器人向下移动的能力
     function moveDown() {
         $this->y -= 1;
-    }
-
-    //记录机器人移动过的位置
-    function savePath() {
-        array_push($this->path, [
-            "x" => $this->x, "y" => $this->y
-        ]);
     }
 }
--- arithmetic/robotSearchPath/searchPath.Class.php
+++ arithmetic/robotSearchPath/searchPath.Class.php
@@ -4,44 +4,91 @@
 //第一次移动为向后移动，并且机器人不会重复清扫同一个地方
 //要求：显示机器人第十二次移动的路径(第几次同理)
 
+//path 是多条的，因此不能固定
+//nextPosition 也是动态变化，因此不能固定
+/*$this->nextPostion = [
+    "Down" => ["x" => $this->x, "y" => $this->y - 1],
+    "Left" => ["x" => $this->x - 1, "y" => $this->y],
+    "Right" => ["x" => $this->x + 1, "y" => $this->y],
+    "Up" => ["x" => $this->x, "y" => $this->y + 1]
+];*/
+
 include_once "./Robot.class.php";
 const TIMES = 12;
 
 class Path {
 
-    public $robot;
-
-    //每次移动的方向数组
+    public $robot;     //移动主体
+    public $multiPath; //所有可能移动的路径
+    
     function __construct() {
         $this->robot = new Robot();
+        $this->multiPath = [];
     }
 
     //判断是否可以往某个方向移动
-    function canMove($nextPosition) {
-        
-        foreach ($this->robot->path as $k => $v) {
+    function canMove($path, $nextPosition) {
+
+        foreach ($path as $k => $v) {
+
             $diff = array_diff($v, $nextPosition);
-            if (0 == count($diff)) {
-                return 0; //下一个移动的位置不符合规则
+
+            //符合不重复规则
+            if (count($diff) == 0) {
+                return 1;
             }
         }
-        return 1;
+        return 0;
+    }
+
+    //更新机器人位置
+    function updatePosition() {
+
+        foreach ($this->robot->nextPostion as $k => $v) {
+            if ($this->canMove($v)) {
+                $operation = "move".$k;
+                $this->robot->$operation();
+                
+                break;
+            }
+        }
+
+
     }
 
     //一定次数下自动寻找路径
-    function searchPath($times = 0) {
+    function searchPath($path = []) {
 
-        if (!$times) die("原地不动");
+        
+        $this->searchPath($path)
+        array_push($this->multiPath, $path);
+    }
 
-        for ($i = 1; $i <= $times; $i++) {
-            foreach ($this->robot->direction as $k => $v) {
-                if ($this->canMove($v)) {
-                    $this->robot->savePath();
-                }
-            }
+    //走起
+    function go($times) {
+        for ($i=0; $i < $times; $i++) { 
+            $this->searchPath();
         }
     }
+
+    //输出路径
+    function echoPath() {
+        echo "<pre>";
+        var_dump($this->multiPath);
+        //用圆形代表机器人
+        /*foreach ($this->robot->path as $k => $v) {
+            for ($i = 0; $i < abs($v['x']); $i++) {
+                echo " ";
+            }
+            for ($j = 0; $j < abs($v['y']); $j++) {
+                echo "<br/>";
+            }
+            echo "o"
+        }*/
+    }
+
+
 }
 
 $aPath = new Path();
-$aPath->searchPath(TIMES);
+$aPath->go(TIMES);
