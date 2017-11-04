d56fbf7dffcfcec901610f410df61f4f0e0d0e0b
huisoido <584331125@qq.com>
2017-11-04 11:37:11 +0800

    docs:2-6

---
+++ ModernPHP/2/2-6.php
@@ -0,0 +1,17 @@
+<?php
+class DocumentStore
+{
+    protected $data = [];
+
+    public function addDocument(Documenttable $document)
+    {
+        $key = $document->getId();
+        $value = $document->getContent();
+        $this->data[$key] = $value;
+    }
+
+    public function getDocuments()
+    {
+        return $this->data;
+    }
+}
