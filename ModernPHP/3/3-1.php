ac76dae78602d058ff2e83ec2671b0f9caa75930
huisoido <584331125@qq.com>
2017-11-07 22:15:41 +0800

    docs:PSR-3

---
+++ ModernPHP/3/3-1.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('myApp');
+$log->pushHandler(new StreamHandler('logs/development.log', Logger::DEBUG));
+$log->pushHandler(new StreamHandler('logs/production.log', Logger::WARNING));
+
+$log->debug('This is a debug message');
+$log->warning('This is a warning message');
