aa41d589ae48183fb95e98b11195b88df416281e
huisoido <584331125@qq.com>
2017-12-01 17:39:20 +0800

    修改PHP引用变量例子

--- others/chapter1/demo1.php
+++ others/chapter1/demo1.php
@@ -6,14 +6,17 @@ xdebug_debug_zval('a');
 $b = $a;
 xdebug_debug_zval('a');
 
+$b = range(0, 1);
+var_dump($b);
+xdebug_debug_zval('a');
+
+
 $a = range(0, 3);
 xdebug_debug_zval('a');
 
 $b = &$a;
 xdebug_debug_zval('a');
 
-/* 运行结果:
-a: (refcount=1, is_ref=0)=array (0 => (refcount=1, is_ref=0)=0, 1 => (refcount=1, is_ref=0)=1, 2 => (refcount=1, is_ref=0)=2, 3 => (refcount=1, is_ref=0)=3)
-a: (refcount=2, is_ref=0)=array (0 => (refcount=1, is_ref=0)=0, 1 => (refcount=1, is_ref=0)=1, 2 => (refcount=1, is_ref=0)=2, 3 => (refcount=1, is_ref=0)=3)
-a: (refcount=1, is_ref=0)=array (0 => (refcount=1, is_ref=0)=0, 1 => (refcount=1, is_ref=0)=1, 2 => (refcount=1, is_ref=0)=2, 3 => (refcount=1, is_ref=0)=3)
-a: (refcount=2, is_ref=1)=array (0 => (refcount=1, is_ref=0)=0, 1 => (refcount=1, is_ref=0)=1, 2 => (refcount=1, is_ref=0)=2, 3 => (refcount=1, is_ref=0)=3) */
+$b = range(0, 1);
+var_dump($b, $a);
+xdebug_debug_zval('a');
