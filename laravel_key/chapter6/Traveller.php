a4e8c20830b4672dced3a9b35ec42c4d870a5b17
huisoido <584331125@qq.com>
2018-11-21 16:29:10 +0800

    study

---
+++ laravel_key/chapter6/Traveller.php
@@ -0,0 +1,52 @@
+<?php
+//设计公共接口
+interface Visit
+{
+    public function go();
+}
+
+//实现不同交通工具类
+class Leg implements Visit
+{
+    public function go()
+    {
+        echo "walk to Tibet!";
+    }
+}
+
+class Car implements Visit
+{
+    public function go()
+    {
+        echo "drive car to Tibet!";
+    }
+}
+
+class Train implements Visit
+{
+    public function go()
+    {
+        echo "go to Tibet by train!";
+    }
+}
+
+//设计旅游者类，该类在实现游西藏的功能时要依赖交通工具实例
+class Traveller
+{
+    protected $trafficTool;
+    public function __construct(Visit $trafficTool)
+    {
+        //依赖产生
+        $this->trafficTool = $trafficTool;
+    }
+
+    public function visitTibet()
+    {
+        $this->trafficTool->go();
+    }
+}
+
+//手动依赖注入
+$trafficTool = new Leg();
+$tra = new Traveller($trafficTool);
+$tra->visitTibet();
